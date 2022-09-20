5ef7c00ba2d28ef3d5e66df82b3a9fdd0d186c72
Eduardo Hobold <hobold.edu@hotmail.com>
2022-09-20 17:10:36 -0300

    Criando autenticação com o Google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Alert } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
 
 import AppleSvg from '../../assets/apple.svg';
@@ -20,8 +21,16 @@ import {
 } from './styles';
 
 export function SignIn() {
-    const { user } = useAuth();
-    console.log(user);
+    const { signInWithGoogle } = useAuth();
+
+    async function handleSignInWithGoogle() {
+        try {
+            await signInWithGoogle();
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Não foi possível conectar a conta Google');
+        }
+    }
 
     return (
         <Container>
@@ -48,6 +57,7 @@ export function SignIn() {
                     <SignInSocialButton 
                         title="Entrar com Google"
                         svg={GoogleSvg}
+                        onPress={handleSignInWithGoogle}
                     />
 
                     <SignInSocialButton 
